d3476aca836c1da53477bf4933d23c0b8c762983
Sadiq <samin.sada@gmail.com>
2021-03-10 13:42:32 -0500

    login page done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/login';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
