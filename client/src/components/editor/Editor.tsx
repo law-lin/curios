62f1ec52491c45c9c259602577c2f86bcafcd5ec
Md Sadiq Sada <samin.sada@gmail.com>
2021-09-24 11:00:07 -0400

    modularize svg icons for editor

--- client/src/components/editor/Editor.tsx
+++ client/src/components/editor/Editor.tsx
@@ -3,9 +3,9 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Highlight from '@tiptap/extension-highlight';
 import Typography from '@tiptap/extension-typography';
-import { Button, Image } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 
-import { ReactComponent as BoldSvg } from '../../assets/bold.svg';
+import Icons from '../../assets/icons';
 
 import './styles.scss';
 
@@ -15,112 +15,70 @@ const MenuBar = ({ editor }) => {
   }
 
   return (
-    <>
+    <Flex flexWrap='wrap'>
       <Button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        <BoldSvg /> 
+        onClick={() => editor.chain().focus().toggleBold().run()}      >
+        <Icons.BoldIcon /> 
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
       >
-        italic
+        <Icons.ItalicIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? 'is-active' : ''}
       >
-        strike
+        <Icons.StrikethoughIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleCode().run()}
-        className={editor.isActive('code') ? 'is-active' : ''}
       >
-        code
-      </Button>
-      <Button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </Button>
-      <Button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
+        <Icons.CodeIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
       >
-        paragraph
+        <Icons.ParagraphIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
       >
-        h1
+        <Icons.HeadingOneIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
       >
-        h2
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-      >
-        h4
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
-      >
-        h5
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
-      >
-        h6
+        <Icons.HeadingTwoIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
       >
-        bullet list
+        <Icons.BulletListIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
       >
-        ordered list
+        <Icons.OrderedListIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
       >
-        code block
+        <Icons.CodeBlockIcon />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
       >
-        blockquote
+        <Icons.BlockquoteIcon />
       </Button>
       <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
+        <Icons.HorizontalRuleIcon />
       </Button>
       <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
+        <Icons.HardBreakIcon />
       </Button>
-      <Button onClick={() => editor.chain().focus().undo().run()}>undo</Button>
-      <Button onClick={() => editor.chain().focus().redo().run()}>redo</Button>
-    </>
+      <Button onClick={() => editor.chain().focus().undo().run()}><Icons.UndoIcon /></Button>
+      <Button onClick={() => editor.chain().focus().redo().run()}><Icons.RedoIcon /></Button>
+    </Flex>
   );
 };
 
